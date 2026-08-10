0770b8d18e5a59d69900e3520b47f62888645a1d
Jimmy <mohamedelashaal.3@gmail.com>
2026-08-10 08:05:23 +0300

    add assignment 1 file

--- main.js
+++ main.js
@@ -1,42 +1,30 @@
 
-// var user= {
-//     name:"ahmed",
-//     age: 10,
-//     gender :"male",
-//     getage:function(){
-        
-
-//         var inner= ()=>{
-//             console.log("inner func : " +this.gender);
-            
-//         }
-//         inner()
-//         return this.age;
-//     }
-// };
-// console.log(user.getage());
-var arr=[1,2,3,4,5];
- res= arr.map(function(value,index,obj){
-    console.log(value);
-    console.log(index);
-    console.log(obj);
-return value *2
-  })
-console.log("this is final result ");
-console.log(res);
+function sum({num1=1,num2=2,num3=3}){
+    
+    console.log("nums");
+    console.log("1",num1);
+    console.log("2",num2);
+    console.log("3",num3);
+    
+    return num1+num2+num3
+}
 
 
+var object={
+    num1 : 1 ,
+    num2 : 3,
+    num4 : 5
+}
+var {num1,num2:numtwo,num3}=object
+console.log(num1,object.num4,numtwo,num3);
 
 
-
-
-
-
-
-
-
-
-
-
+ 
+{var x=10
+    
+    console.log(x);
+}
+console.log(x);
+0
 
 
